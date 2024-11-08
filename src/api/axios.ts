228441a4d596c1e884d0a000ee499b4d51636759
firos <vpzfiroz@gmail.com>
2024-11-08 19:57:24 +0530

    updated

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://taskify-backend-qpae.onrender.com",
     withCredentials:true
 })
 
